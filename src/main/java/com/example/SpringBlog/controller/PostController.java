304fe96b6a366feceb2eceb0927b5bac3aa52a30
rlaxodn <taewoo3728@naver.com>
2024-05-12 23:43:36 +0900

    2st commit

--- src/main/java/com/example/SpringBlog/controller/PostController.java
+++ src/main/java/com/example/SpringBlog/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.example.SpringBlog.controller;
 
 import com.example.SpringBlog.dto.PostDTO;
+import com.example.SpringBlog.entity.PostEntity;
+import com.example.SpringBlog.repository.PostRepository;
 import com.example.SpringBlog.repository.UserRepository;
 import com.example.SpringBlog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PostController {
@@ -16,7 +22,7 @@ public class PostController {
     @Autowired
     PostService postService;
     @Autowired
-    UserRepository userRepository;
+    PostRepository postRepository;
 
     @GetMapping("/post")
     public String post(Model model){
@@ -34,5 +40,28 @@ public class PostController {
 
         return "redirect:/";
 
+    }
+    @GetMapping("/postList")
+    public String postList(Model model){
+
+        List<PostEntity> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
+
+        return "postList";
+    }
+
+    @GetMapping("/post/{id}")
+    public String postDetail(@PathVariable("id") int id, Model model){
+
+        //postEntity에서 ID로 게시글 정보를 찾음
+        Optional<PostEntity> postOptional = postRepository.findById(id);
+        if (postOptional.isPresent()) {
+            PostEntity post = postOptional.get();
+            model.addAttribute("post", post);
+            return "postDetail";
+        } else {
+            // 게시글이 없을 경우 처리
+            return "redirect:/postList";
+        }
     }
 }
